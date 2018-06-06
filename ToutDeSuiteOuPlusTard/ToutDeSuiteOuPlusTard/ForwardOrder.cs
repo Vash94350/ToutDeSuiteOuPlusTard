b363194dc5a748c2d2282a9eb95ae3df78338d37
Vash94350 <michael.pean.mp@gmail.com>
2018-06-06 20:12:52 +0200

    creation du projet

---
+++ ToutDeSuiteOuPlusTard/ToutDeSuiteOuPlusTard/ForwardOrder.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ToutDeSuiteOuPlusTard
+{
+    class ForwardOrder : Order
+    {
+        public void Pay()
+        {
+
+        }
+
+        public bool IsValid()
+        {
+            return false;
+        }
+    }
+}
---
+++ ToutDeSuiteOuPlusTard/ToutDeSuiteOuPlusTard/OrderCreator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ToutDeSuiteOuPlusTard
+{
+    class OrderCreator
+    {
+        public void CreateNewOrder()
+        {
+            Order order = CreateOrder();
+            if(order.IsValid())
+            {
+                order.Pay();
+            }
+        }
+
+        private Order CreateOrder()
+        {
+            if(paymentType == PaymentType.Spot)
+            {
+                return new SpotOrder();
+            }
+            else
+            {
+                return new ForwardOrder();
+            }
+        }
+    }
+}
